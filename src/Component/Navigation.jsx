ee6ee5643b99bf7eaf5c3463dba0ecc4939df3eb
chandiosamad07 <absamadchandio@gmail.com>
2023-08-17 23:51:10 +0500

    Nav bar

---
+++ src/Component/Navigation.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Navigation() {
+  return (
+    <nav>
+        <div className='logo'>
+            <img src='/asset/logo.png' alt=''></img>
+        </div>
+
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+      
+    </nav>
+  )
+}
+
+export default Navigation
